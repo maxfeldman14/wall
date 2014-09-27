d3e6013e66d92bc48808775fd979a951debe93a3
Ram Kandasamy <ram.kandasamy@gmail.com>
2014-09-27 00:50:06 -0700

    A bunch of print statements :D

--- LEDs/serial/src/sketch.ino
+++ LEDs/serial/src/sketch.ino
@@ -26,22 +26,34 @@ void readByte() {
   case -3:
     if (byte == 13) {
       frame_index++;
+      Serial.write("Got 13");
     }
     return;
   case -2:
     if (byte == 37) {
       frame_index++;
+      Serial.write("Got 37");
     } else {
       frame_index = -3;
     }
     return;
   case -1:
-    if (byte > 0 && byte <= 20)
-    frame_size = byte;
+    if (byte > 0 && byte <= 20) {
+      frame_size = byte;
+      Serial.write("Got length:");
+      Serial.println(byte);
+    } else {
+      frame_size = 1;
+      Serial.write("Got invalid length:");
+    }
     frame_index++;
     return;
   default:
     frame[frame_index] = byte;
+    Serial.write("Got byte:");
+    Serial.println(byte);
+    Serial.write("led: ");
+    Serial.println(led(frame_index));
     frame_index++;
     frame_index = frame_index == frame_size ? -3 : frame_index;
     break;
@@ -52,5 +64,8 @@ void drawFrame() {
   int thresh = micros() % 256;
   for (int i = 0; i < frame_size; i++) {
     digitalWrite(led(i), frame[i] > thresh ? HIGH : LOW);
+    if (led(i) == 22) {
+      Serial.println(frame[i] > thresh ? "HIGH" : "LOW");
+    }
   }
 }
